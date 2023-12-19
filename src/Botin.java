1e0e0b4fe21176be1a5369260e2f9836799e0ce8
RobertoPecurto <robpecesc@alu.edu.gva.es>
2023-12-19 10:42:20 +0100

    Más ejercicio

--- src/Botin.java
+++ src/Botin.java
@@ -4,18 +4,25 @@ import java.util.HashMap;
 
 public class Botin {
     public static void main(String[] args) {
-        int[] billetes = {2,10,20,50,200,500};
-        int participantes = billetes[0];
-        System.out.println(repartir(participantes, billetes));
+        System.out.println(repartir(2, 10, 20, 50, 200, 500));;
     }
 
     public static String repartir (int participantes, int ... billetes){
         HashMap<Integer, ArrayList<Integer>> rep = new HashMap<>();
-        ArrayList<Integer> billetesrep = new ArrayList<>();
-        for (int i = 0; i < rep.size(); i++) {
-            billetesrep.add(billetes[i % participantes]);
-        }
+        for (int i = 0; i < participantes; i++) {
+            ArrayList<Integer> billetesrep = new ArrayList<>();
+            for (int j = 0; j < billetes.length; j++) {
+                int contador = 0;
+                if (j==0){
 
-        return billetesrep.toString();
+                    billetesrep.add(billetes[0]);
+                }else {
+                    billetesrep.add(billetes[contador]);
+                    contador += participantes;
+                }
+            }
+            rep.put(i,billetesrep);
+        }
+        return rep.toString();
     }
 }
