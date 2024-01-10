b0359d693eba02b85021a85c6824c34e852d55b9
RobertoPecurto <robpecesc@alu.edu.gva.es>
2024-01-10 09:59:34 +0100

    AddAll

--- src/AddAll.java
+++ src/AddAll.java
@@ -1,7 +1,18 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class AddAll {
     public static void main(String[] args) {
-        ArrayList<Integer> lista = new ArrayList<>();
+        List<String> nums = new ArrayList<>();
+        nums.add("uno");
+        nums.add("dos");
+        nums.add("tres");
+        List<String> nums2 = new ArrayList<>();
+        nums2.add("cuatro");
+        nums2.add("cinco");
+        nums2.add("seis");
+
+        nums.addAll(nums2);
+        System.out.println(nums);
     }
 }
