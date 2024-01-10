ff630a5d962fe40623ad5e012b29f1e1027161d4
RobertoPecurto <robpecesc@alu.edu.gva.es>
2024-01-10 10:16:08 +0100

    AddAll

--- src/Parentesis.java
+++ src/Parentesis.java
@@ -2,8 +2,6 @@ import java.util.Stack;
 
 public class Parentesis {
     public static void main(String[] args) {
-        //Regular. 
-        /* por ejemplo (()) dice que está mal */
         String cadena = Utilidades.leerCadena("Introduce una cadena:");
         if (esParentizado(cadena)){
             System.out.println("La cadena está bien parentizada");
@@ -19,17 +17,16 @@ public class Parentesis {
             if (letra == '(' || letra == '[' || letra == '{'){
                 pila.add(letra);
             } else if (letra == ')' || letra == ']' || letra == '}') {
-                char ultimo = pila.pop();
                 if (pila.isEmpty()) {
                     return false;
                 }
-                if (ultimo == ')' && letra != '(') return false;
-                if (ultimo == ']' && letra != '[') return false;
-                if (ultimo == '}' && letra != '{') return false;
-                if (pila.size() == 1) return false;
+                char ultimo = pila.pop();
+                if (ultimo == '(' && letra != ')') return false;
+                if (ultimo == '[' && letra != ']') return false;
+                if (ultimo == '{' && letra != '}') return false;
             }
 
         }
-        return true;
+        return pila.isEmpty();
     }
 }
